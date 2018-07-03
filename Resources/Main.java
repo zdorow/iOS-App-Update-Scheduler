0bf742ba0755f5125a931b3161bc19b9f398de9b
Zach Dorow <zdorow@me.com>
2018-07-03 00:59:28 -0700

    Getting setup for setup script.

---
+++ Resources/Main.java
@@ -0,0 +1,25 @@
+package com.app.update.scheduler;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/app_update_schedule.fxml"));
+		
+		Scene scene = new Scene(root, 550, 325);
+		
+		primaryStage.setTitle("App Update Scheduler");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
